7e32ba1c88dc0bfba1d63f07cffec1598324d6db
Marcelo Saad <mc.saad@hotmail.com>
2020-10-03 20:38:34 -0300

    Implemented post and get middleware for PATH users

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require("express");
 const app = express();
 
+//Simular um banco de dados de usuários:
+const users = []; //
+let id = 1;
 
-app.use("/", (req, res) => {
-    res.send("Middlewear 1");
+
+app.use(express.json()); //middlewear que a gente bota antes pra que a API consiga entender requests de json (ela faz um ".parseJSON")
+
+app.post("/users", (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    //ou então apenas ===> const {name:name, email:email} = req.body;
+    //e caso a sua chave e o seu valor tenham a mesma assinatura, pode simplificar ainda mais ===> const {name, email} = req.body;
+    const newUser = {
+        id:id,
+        name:name,
+        email:email,
+        createdAt:Date.now().toString()
+    };
+    users.push(newUser);
+    id++;
 })
+
+app.get("/users", (req,res) => {
+    console.log('get users request');
+    console.log(users);
+    res.json(users)
+})
+
+
 app.listen(5000, () => {
     console.log("Server running on Port 5000");
 })
